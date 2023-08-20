9170739bef0165934ba39eada5728626c6f29f97
mosescode1 <emosescode@gmail.com>
2023-08-20 13:28:44 +0100

    comment out the error code status

--- lazyPush/git-push.sh
+++ lazyPush/git-push.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
 git add .
-read -p "Enter The Commit: " -e -t 20 message
+read -p "Enter The Commit: " -e -t 60 message
 git commit -m "$message"
 git push
 
 # Capture the exit status of the git push command
-
-exit_status=$?
+#exit_status=$?
 
 # Check the exit status and take appropriate actions
-if [ $exit_status -eq 0 ]; then
-	echo "Push successful."
-elif [ $exit_status -eq 1 ]; then
-	echo "Generic error occurred during git push."
-elif [ $exit_status -eq 2 ]; then
-	echo "Permission denied or repository unavailable."
-elif [ $exit_status -eq 128 ]; then
-	echo "Fatal error occurred during git push."
-elif [ $exit_status -ge 128 ] && [ $exit_status -le 192 ]; then
-	echo "Non-fast-forward rejection or other git push error (Exit code: $exit_status)."
-elif [ $exit_status -eq 130 ]; then
-	echo "Operation canceled."
-elif [ $exit_status -eq 131 ]; then
-	echo "Remote hang-up."
-elif [ $exit_status -eq 141 ]; then
-	echo "Pipe broken."
-elif [ $exit_status -eq 143 ]; then
-	echo "Operation terminated."
-else
-	echo "Unknown exit code: $exit_status"
-fi
+#if [ $exit_status -eq 0 ]; then
+#	echo "Push successful."
+#elif [ $exit_status -eq 1 ]; then
+#	echo "Generic error occurred during git push."
+#elif [ $exit_status -eq 2 ]; then
+#	echo "Permission denied or repository unavailable."
+#elif [ $exit_status -eq 128 ]; then
+#	echo "Fatal error occurred during git push."
+#elif [ $exit_status -ge 128 ] && [ $exit_status -le 192 ]; then
+#	echo "Non-fast-forward rejection or other git push error (Exit code: $exit_status)."
+#elif [ $exit_status -eq 130 ]; then
+#	echo "Operation canceled."
+#elif [ $exit_status -eq 131 ]; then
+#	echo "Remote hang-up."
+#elif [ $exit_status -eq 141 ]; then
+#	echo "Pipe broken."
+#elif [ $exit_status -eq 143 ]; then
+#	echo "Operation terminated."
+#else
+#	echo "Unknown exit code: $exit_status"
+#fi   ***/
