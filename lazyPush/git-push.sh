cc9fd45e0da6218c24486997c2a78331ee66e743
mosescode1 <emosescode@gmail.com>
2023-08-20 02:19:30 +0100

    hh

--- lazyPush/git-push.sh
+++ lazyPush/git-push.sh
@@ -3,4 +3,12 @@
 git add .
 read -p "Enter The Commit: " message
 git commit -m "$message"
-git push
+
+output=$(git push 2>&1)
+
+# Check if the output contains the word "error"
+if echo "$output" | grep -i "error"; then
+	echo "An error was detected in the output."
+else
+	echo "Push Successful"
+fi
